b32d02a6842d072bb8d7a62e8e279bbaa7dae281
Aarushkumartiwari <79394257+Aarushkumartiwari@users.noreply.github.com>
2021-05-20 19:18:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,188 @@
+var cartValue = document.getElementById("cart-value") ;
+var cartButton = document.getElementById("cart") ;
+var  addButtons = document.getElementById("button") ;
+
+var addButtons=document.getElementsByClassName("button") ;
+
+
+            
+
+
+ var  items=[
+
+
+{
+    name:"This was pact" ,
+    quantity : 0,
+    dollars : 7,
+    cents :49,
+} ,
+
+
+{
+    name:"The famous five " ,
+    quantity : 0,
+    dollars : 4 ,
+    cents :59,
+} ,
+
+
+{
+    name:"Matlida" ,
+    quantity : 0,
+    dollars :  6,
+    cents :80 ,
+} ,
+
+
+{
+    name:"Harry potter" ,
+    quantity : 0,
+    dollars : 10,
+    cents :0 ,
+} ,
+
+ {
+    name:"For Young Readers" ,
+    quantity : 0,
+    dollars : 7 ,
+    cents : 29 ,
+} ,
+
+
+{
+    name:"Wimpy Kid - DIY" ,
+    quantity : 0,
+    dollars : 4,
+    cents :99,
+} ,
+
+{
+    name:"Dart Board" ,
+    quantity : 0,
+    dollars : 17 ,
+    cents :49,
+} ,
+
+
+{
+    name:"Connect 4" ,
+    quantity : 0,
+    dollars : 19 ,
+    cents : 99,
+} ,
+
+
+{
+    name:"Jenga" ,
+    quantity : 0,
+    dollars :20  ,
+    cents : 99 ,
+} ,
+
+
+{
+    name:"Monopoly" ,
+    quantity : 0,
+    dollars : 19 ,
+    cents : 49 ,
+} ,
+
+
+
+
+{
+    name:"BOOK MARKS" ,
+    quantity : 0,
+    dollars : 14 ,
+    cents : 29 ,
+} ,
+
+
+{
+    name: " Birthday Card " ,
+    quantity : 0,
+    dollars : 19 ,
+    cents : 99 ,
+} ,
+
+
+{
+    name:"Stuffed toys" ,
+    quantity : 0,
+    dollars : 15 ,
+    cents : 99 ,
+} ,
+
+
+{
+    name:"Dream catcher drawing" ,
+    quantity : 0,
+    dollars : 18 ,
+    cents : 49  ,
+} ,
+
+] ;
+
+
+
+function updateCart() {
+    let cart = 0 ; 
+    for(index = 0 ; index < items.length; index++){
+         cart = cart + items[index].quantity;
+    }
+    cartValue.innerHTML = cart ;
+}
+
+ 
+
+ for(let i = 0; i < addButtons.length; i++){
+    addButtons[i].onclick = (e) => {
+    items[i].quantity++ ;
+    updateCart() ;
+} ; 
+
+
+ }
+
+
+
+
+var finalDollars = 0 ;
+var finalCents = 0 ;
+ 
+   function updatePrice(){
+       let totalPriceInCents = 0 ;
+
+       for(index=0; index<items.length ;index++){
+           totalPriceInCents=totalPriceInCents+items[index].quantity+[items[index].dollars+100+items[index].cents]
+       }
+                  finalDollars=Math.floor(totalPriceInCents / 100) ;
+             
+   finalCents=totalPriceInCents % 100 ;
+   }
+
+         
+    
+
+                                                                                                           
+ cartButton.onclick=()=>{
+    updatePrice() ;
+    updateWhatsappLink() ;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
